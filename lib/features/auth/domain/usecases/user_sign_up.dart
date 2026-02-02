0481e73343083d7bc9247d8622622845cd76ef39
deeepak-me <deepak.krishnan.e@gmail.com>
2026-02-02 23:25:21 +0530

    feat: user sign up implemented

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,30 @@
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
+
+class UserSignUp implements Usecase<User, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(UserSignUpParams Params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: Params.name,
+      email: Params.email,
+      password: Params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+  UserSignUpParams({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+}
