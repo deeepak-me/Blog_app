1fdf60dff2d0cf12c36b3127c8adc08b1cb9a9d6
deeepak-me <deepak.krishnan.e@gmail.com>
2026-02-06 21:27:09 +0530

    feat: blog upload class implemented

---
+++ lib/features/blog/domain/usecases/upload_blog.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UploadBlog implements Usecase<Blog, UploadBlogParams> {
+  final BlogRepository blogRepository;
+  UploadBlog(this.blogRepository);
+  @override
+  Future<Either<Failure, Blog>> call(UploadBlogParams Params) async {
+    return await blogRepository.uploadBlog(
+      image: Params.image,
+      title: Params.title,
+      content: Params.content,
+      posterId: Params.posterId,
+      topics: Params.topics,
+    );
+  }
+}
+
+class UploadBlogParams {
+  final String posterId;
+  final String title;
+  final String content;
+  final File image;
+  final List<String> topics;
+
+  UploadBlogParams({
+    required this.posterId,
+    required this.title,
+    required this.content,
+    required this.image,
+    required this.topics,
+  });
+}
