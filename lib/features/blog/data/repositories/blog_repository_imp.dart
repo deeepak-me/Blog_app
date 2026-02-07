5ae811e38b5098f9cdca3c7025f2cfa73d75aa97
deeepak-me <deepak.krishnan.e@gmail.com>
2026-02-07 18:50:14 +0530

    refactor: connection message updated with constants

--- lib/features/blog/data/repositories/blog_repository_imp.dart
+++ lib/features/blog/data/repositories/blog_repository_imp.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:blog_app/core/constants/constants.dart';
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/core/network/connection_checker.dart';
@@ -30,7 +31,7 @@ class BlogRepositoryImp implements BlogRepository {
   }) async {
     try {
       if (!await (connectionChecker.isConnected)) {
-        return left(Failure('No Internet Connection!'));
+        return left(Failure(Constants.noConnectionErrorMessage));
       }
       BlogModel blogModel = BlogModel(
         id: const Uuid().v1(),
